bb5c52fdad5585ad272288920ec49952a18d1ea4
Varad Deshmukh <vd876733@gmail.com>
2026-06-22 19:46:41 +0530

    Time: 0 ms (100%), Space: 8.3 MB (51.01%) - LeetHub

--- 0551-student-attendance-record-i/0551-student-attendance-record-i.cpp
+++ 0551-student-attendance-record-i/0551-student-attendance-record-i.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
     bool checkRecord(string s) {
-
         int absent = 0;
         int late = 0;
-
         for(int i = 0; i < s.size(); i++) {
-
             if(s[i] == 'A') {
                 absent++;
             }
-
             if(s[i] == 'L') {
                 late++;
             }
             else {
                 late = 0;
             }
-
             if(absent >= 2 || late >= 3) {
                 return false;
             }
