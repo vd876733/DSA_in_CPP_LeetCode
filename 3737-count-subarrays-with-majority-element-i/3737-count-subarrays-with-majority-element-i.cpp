5e8600738e8d6266dadf1dd846c7f25b17d7c38e
Varad Deshmukh <vd876733@gmail.com>
2026-06-25 23:52:55 +0530

    Time: 11 ms (92.81%), Space: 42.6 MB (22%) - LeetHub

--- 3737-count-subarrays-with-majority-element-i/3737-count-subarrays-with-majority-element-i.cpp
+++ 3737-count-subarrays-with-majority-element-i/3737-count-subarrays-with-majority-element-i.cpp
@@ -1,20 +1,53 @@
 class Solution {
 public:
     int countMajoritySubarrays(vector<int>& nums, int target) {
+
         int n = nums.size();
-        int ans = 0;
+
+        vector<int> prefix(n + 1, 0);
+
         for(int i = 0; i < n; i++) {
-            int count = 0;
-            for(int j = i; j < n; j++) {
-                if(nums[j] == target) {
-                    count++;
-                }
-                int len = j - i + 1;
-                if(count > len / 2) {
-                    ans++;
-                }
-            }
+            if(nums[i] == target)
+                prefix[i + 1] = prefix[i] + 1;
+            else
+                prefix[i + 1] = prefix[i] - 1;
         }
+
+        vector<int> vals = prefix;
+        sort(vals.begin(), vals.end());
+        vals.erase(unique(vals.begin(), vals.end()), vals.end());
+
+        vector<int> bit(vals.size() + 1, 0);
+
+        auto update = [&](int idx) {
+            while(idx < bit.size()) {
+                bit[idx]++;
+                idx += idx & (-idx);
+            }
+        };
+
+        auto query = [&](int idx) {
+            int sum = 0;
+            while(idx > 0) {
+                sum += bit[idx];
+                idx -= idx & (-idx);
+            }
+            return sum;
+        };
+
+        long long ans = 0;
+
+        for(int x : prefix) {
+
+            int idx =
+                lower_bound(vals.begin(), vals.end(), x)
+                - vals.begin() + 1;
+
+            ans += query(idx - 1);
+
+            update(idx);
+        }
+
         return ans;
     }
 };
