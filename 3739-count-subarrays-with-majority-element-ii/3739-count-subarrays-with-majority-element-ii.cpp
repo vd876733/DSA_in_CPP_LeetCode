d6266c54cb1219da3e4c0d171c72e514cf0768f2
Varad Deshmukh <vd876733@gmail.com>
2026-06-26 19:51:08 +0530

    Time: 5 ms (92.72%), Space: 95.5 MB (92.71%) - LeetHub

--- 3739-count-subarrays-with-majority-element-ii/3739-count-subarrays-with-majority-element-ii.cpp
+++ 3739-count-subarrays-with-majority-element-ii/3739-count-subarrays-with-majority-element-ii.cpp
@@ -2,12 +2,9 @@ class Solution {
 public:
     long long countMajoritySubarrays(vector<int>& nums, int target) {
         int size = nums.size(), pref = size;
-
         vector<int> freq(2 * size + 1);
         freq[size] = 1;
-
         long long less = 0, ans = 0;
-
         for (int num : nums) {
             if (num == target)
                 less += freq[pref++];
@@ -17,7 +14,6 @@ public:
             ++freq[pref];
             ans += less;
         }
-
         return ans;
     }
 };
