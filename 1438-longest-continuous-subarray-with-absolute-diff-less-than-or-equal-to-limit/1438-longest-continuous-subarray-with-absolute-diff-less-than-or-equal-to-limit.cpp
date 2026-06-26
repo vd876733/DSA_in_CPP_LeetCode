8ff0490b6518e34ff9c651163574d5bceabf27c9
Varad Deshmukh <vd876733@gmail.com>
2026-06-27 02:00:49 +0530

    Time: 23 ms (64.44%), Space: 77.6 MB (47.81%) - LeetHub

--- 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
@@ -6,14 +6,11 @@ public:
         int left = 0;
         int ans = 0;
         for(int right = 0; right < nums.size(); right++) {
-            while(!maxdq.empty() &&
-                  nums[maxdq.back()] < nums[right]) {
-
+            while(!maxdq.empty() && nums[maxdq.back()] < nums[right]) {
                 maxdq.pop_back();
             }
             maxdq.push_back(right);
-            while(!mindq.empty() &&
-                  nums[mindq.back()] > nums[right]) {
+            while(!mindq.empty() && nums[mindq.back()] > nums[right]) {
                 mindq.pop_back();
             }
             mindq.push_back(right);
