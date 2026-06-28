0d7c18ee412aeb370914e9561841597437444369
Varad Deshmukh <vd876733@gmail.com>
2026-06-29 00:49:25 +0530

    Time: 39 ms (58.21%), Space: 89.4 MB (30%) - LeetHub

--- 3097-shortest-subarray-with-or-at-least-k-ii/3097-shortest-subarray-with-or-at-least-k-ii.cpp
+++ 3097-shortest-subarray-with-or-at-least-k-ii/3097-shortest-subarray-with-or-at-least-k-ii.cpp
@@ -2,44 +2,31 @@ class Solution {
 public:
 
     int minimumSubarrayLength(vector<int>& nums, int k) {
-
         vector<int> bits(32, 0);
-
         int left = 0;
         int ans = INT_MAX;
-
         int currOR = 0;
-
         for(int right = 0; right < nums.size(); right++) {
-
             for(int b = 0; b < 32; b++) {
                 if(nums[right] & (1 << b)) {
                     bits[b]++;
                 }
             }
-
             currOR = 0;
-
             for(int b = 0; b < 32; b++) {
                 if(bits[b] > 0) {
                     currOR |= (1 << b);
                 }
             }
-
             while(left <= right && currOR >= k) {
-
                 ans = min(ans, right - left + 1);
-
                 for(int b = 0; b < 32; b++) {
                     if(nums[left] & (1 << b)) {
                         bits[b]--;
                     }
                 }
-
                 left++;
-
                 currOR = 0;
-
                 for(int b = 0; b < 32; b++) {
                     if(bits[b] > 0) {
                         currOR |= (1 << b);
@@ -47,7 +34,6 @@ public:
                 }
             }
         }
-
         return ans == INT_MAX ? -1 : ans;
     }
 };
