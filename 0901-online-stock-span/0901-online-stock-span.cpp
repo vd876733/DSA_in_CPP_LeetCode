3fe5dc164e5e82ad5eabb13323b61bf5ac3ae9a6
Varad Deshmukh <vd876733@gmail.com>
2026-07-09 11:37:51 +0530

    Time: 28 ms (70.46%), Space: 91.9 MB (90.7%) - LeetHub

---
+++ 0901-online-stock-span/0901-online-stock-span.cpp
@@ -0,0 +1,16 @@
+// C++
+class StockSpanner {
+    stack<pair<int,int>> st;
+public:
+    StockSpanner() {}
+
+    int next(int price) {
+        int span = 1;
+        while (!st.empty() && st.top().first <= price) {
+            span += st.top().second;
+            st.pop();
+        }
+        st.push({price, span});
+        return span;
+    }
+};
