aee50039c0788ac77984242c5c7dbc5d324891d6
Varad Deshmukh <vd876733@gmail.com>
2026-07-17 01:40:15 +0530

    Time: 0 ms (100%), Space: 9.6 MB (92.85%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if( s.size() != t.size()){
+            return false;
+        }
+        int count[26] = {0};
+        for( int i = 0  ; i < s.size() ; i++){
+            count[s[i] - 'a']++;
+        }
+        for( int i  = 0 ; i < t.size() ; i++){
+            if(count[t[i] - 'a'] == 0){
+                return false;
+            }
+            count[t[i] - 'a']--;
+        }
+        return true;
+    }
+};
