e4e4ca73e7391280ee0e77a70ad9d482a027bfd1
Varad Deshmukh <vd876733@gmail.com>
2026-07-17 02:22:17 +0530

    Time: 0 ms (100%), Space: 8.8 MB (41.6%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.cpp
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -1,47 +1,33 @@
 class Solution {
 public:
     bool wordPattern(string pattern, string s) {
-
         vector<string> words;
         string word = "";
 
-        // Split string into words
-        for (int i = 0; i < s.size(); i++) {
-
-            if (s[i] == ' ') {
-
+        for( int i = 0 ; i < s.size() ; i++){
+            if( s[i] == ' '){
                 words.push_back(word);
                 word = "";
-            }
-            else {
-
+            }else{
                 word += s[i];
             }
         }
-
         words.push_back(word);
-
         if (words.size() != pattern.size()) {
             return false;
         }
-
-        unordered_map<char, string> mp1;
-        unordered_map<string, char> mp2;
-
-        for (int i = 0; i < pattern.size(); i++) {
-
-            if (mp1.count(pattern[i]) && mp1[pattern[i]] != words[i]) {
+        unordered_map<char ,string> mp1;
+        unordered_map<string ,char> mp2;
+        for( int i = 0 ; i < pattern.size() ; i++){
+            if(mp1.count(pattern[i]) && mp1[pattern[i]] != words[i]){
+                return false;
+            }else if(mp2.count(words[i]) && mp2[words[i]] != pattern[i]){
                 return false;
             }
-
-            if (mp2.count(words[i]) && mp2[words[i]] != pattern[i]) {
-                return false;
-            }
-
             mp1[pattern[i]] = words[i];
             mp2[words[i]] = pattern[i];
         }
-
         return true;
+        
     }
 };
