bb62dc80486298ab3d8de54c4b5bfdfb6aeeedc5
Varad Deshmukh <vd876733@gmail.com>
2026-07-19 18:57:47 +0530

    Time: 59 ms (9.31%), Space: 75.8 MB (5.2%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -5,7 +5,7 @@ public:
         for( int i = 0 ; i < nums.size() ; i++){
             pq.push(nums[i]);
         }
-        for( int i = 1 ; i < k ; i++){
+        for( int i = 0 ; i < k-1 ; i++){
                 pq.pop();      
         }
         return pq.top();
