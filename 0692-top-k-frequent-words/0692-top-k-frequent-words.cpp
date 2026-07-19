33252e218e73e7a63d0d640540b230dfa2b366c5
Varad Deshmukh <vd876733@gmail.com>
2026-07-19 21:47:04 +0530

    Time: 6 ms (24.57%), Space: 18.1 MB (61.94%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
@@ -1,26 +1,44 @@
 class Solution {
 public:
 
-    static bool cmp(pair<string,int>& a,pair<string,int>& b) {
-        if(a.second == b.second){
-            return a.first < b.first;
+    struct cmp {
+
+        bool operator()(pair<string,int>& a, pair<string,int>& b) {
+
+            if (a.second == b.second) {
+                return a.first > b.first;
+            }
+
+            return a.second < b.second;
         }
-        return a.second > b.second;
-    }
+    };
+
     vector<string> topKFrequent(vector<string>& words, int k) {
+
         unordered_map<string,int> mp;
-        for(int i = 0; i < words.size(); i++) {
+
+        for (int i = 0; i < words.size(); i++) {
             mp[words[i]]++;
         }
-        vector<pair<string,int>> vec;
-        for(auto it : mp) {
-            vec.push_back(it);
+
+        priority_queue<
+            pair<string,int>,
+            vector<pair<string,int>>,
+            cmp
+        > pq;
+
+        for (auto it : mp) {
+            pq.push({it.first, it.second});
         }
-        sort(vec.begin(), vec.end(), cmp);
+
         vector<string> ans;
-        for(int i = 0; i < k; i++) {
-            ans.push_back(vec[i].first);
+
+        while (k--) {
+
+            ans.push_back(pq.top().first);
+            pq.pop();
         }
+
         return ans;
     }
 };
