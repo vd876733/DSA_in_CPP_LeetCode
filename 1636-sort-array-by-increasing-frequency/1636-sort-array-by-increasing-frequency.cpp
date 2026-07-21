88aaa605b3aaf59ea497d21dee8167249542657c
Varad Deshmukh <vd876733@gmail.com>
2026-07-21 16:22:41 +0530

    Time: 3 ms (53.44%), Space: 15 MB (84.78%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
@@ -10,6 +10,39 @@ public:
                 return a > b;      
             return mp[a] < mp[b]; 
         });
-        return nums;
+        return nums;class Solution {
+public:
+    vector<int> frequencySort(vector<int>& nums) {
+
+        unordered_map<int, int> mp;
+
+        for (int i = 0; i < nums.size(); i++) {
+            mp[nums[i]]++;
+        }
+
+        priority_queue<pair<int, int>> pq;
+
+        for (auto it : mp) {
+            pq.push({it.second, -it.first});
+        }
+
+        vector<int> ans;
+
+        while (!pq.empty()) {
+
+            int freq = pq.top().first;
+            int val = -pq.top().second;
+            pq.pop();
+
+            for (int i = 0; i < freq; i++) {
+                ans.push_back(val);
+            }
+        }
+
+        reverse(ans.begin(), ans.end());
+
+        return ans;
+    }
+};
     }
 };
