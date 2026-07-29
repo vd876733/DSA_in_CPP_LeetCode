09bbf465b22cd5205057a2e7fe91b7ba341f1054
Varad Deshmukh <vd876733@gmail.com>
2026-07-29 16:11:35 +0530

    Time: 38 ms (85.89%), Space: 53.5 MB (64.48%) - LeetHub

---
+++ 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    return {
+        toBe: function(expected) {
+            if (val === expected) {
+                return true;
+            }
+            throw new Error("Not Equal");
+        },
+        notToBe: function(expected) {
+            if (val !== expected) {
+                return true;
+            }
+            throw new Error("Equal");
+        }
+    };
+
+};
