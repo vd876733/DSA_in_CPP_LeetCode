35730e446ae8b620ad2b1cc38db0e8d458e7e9fa
Varad Deshmukh <vd876733@gmail.com>
2026-07-30 14:13:29 +0530

    Time: 17 ms (25.58%), Space: 29.9 MB (92.76%) - LeetHub

---
+++ 2116-check-if-a-parentheses-string-can-be-valid/2116-check-if-a-parentheses-string-can-be-valid.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool canBeValid(string s, string locked) {
+        int n = s.size();
+        if (n % 2 == 1)
+            return false;
+        int balance = 0;
+        // Left -> Right
+        for (int i = 0; i < n; i++) {
+            if (locked[i] == '0' || s[i] == '(')
+                balance++;
+            else
+                balance--;
+
+            if (balance < 0)
+                return false;
+        }
+        balance = 0;
+        // Right -> Left
+        for (int i = n - 1; i >= 0; i--) {
+            if (locked[i] == '0' || s[i] == ')')
+                balance++;
+            else
+                balance--;
+
+            if (balance < 0)
+                return false;
+        }
+        return true;
+    }
+};
