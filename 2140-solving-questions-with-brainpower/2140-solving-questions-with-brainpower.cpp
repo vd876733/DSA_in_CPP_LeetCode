aa642b3e15844d443ba82a213f06db8cff7405d7
Varad Deshmukh <vd876733@gmail.com>
2026-08-01 00:50:53 +0530

    Time: 4 ms (76.74%), Space: 119.7 MB (92.17%) - LeetHub

---
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    long long mostPoints(vector<vector<int>>& questions) {
+        int n = questions.size();
+        vector<long long> dp(n + 1, 0);
+        for (int i = n - 1; i >= 0; i--) {
+            long long take = questions[i][0];
+            int next = i + questions[i][1] + 1;
+            if (next < n){
+                take += dp[next];
+            }
+            long long skip = dp[i + 1];
+            dp[i] = max(take, skip);
+        }
+        return dp[0];
+    }
+};
