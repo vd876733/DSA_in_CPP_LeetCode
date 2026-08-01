c0deaa1f6d2f061ce09967d9a37f1af5c147a1a4
Varad Deshmukh <vd876733@gmail.com>
2026-08-01 15:16:21 +0530

    Time: 0 ms (100%), Space: 9.7 MB (69.71%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -2,9 +2,7 @@ class Solution {
 public:
     bool predictTheWinner(vector<int>& nums) {
         int n = nums.size();
-        
         if (n % 2 == 0) return true; 
-        
         vector<int> dp(nums);
         for (int i = n - 2; i >= 0; --i) {
             for (int j = i + 1; j < n; ++j) {
