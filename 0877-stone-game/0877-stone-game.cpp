94ea893fa7464490b93ee059d4297905a6add38e
Varad Deshmukh <vd876733@gmail.com>
2026-08-02 11:51:49 +0530

    Time: 12 ms (23.1%), Space: 19.5 MB (27.79%) - LeetHub

---
+++ 0877-stone-game/0877-stone-game.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+
+        int n = piles.size();
+
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+
+        for (int i = 0; i < n; i++)
+            dp[i][i] = piles[i];
+
+        for (int len = 2; len <= n; len++) {
+
+            for (int l = 0; l + len - 1 < n; l++) {
+
+                int r = l + len - 1;
+
+                int left = piles[l] - dp[l + 1][r];
+
+                int right = piles[r] - dp[l][r - 1];
+
+                dp[l][r] = max(left, right);
+            }
+        }
+
+        return dp[0][n - 1] > 0;
+    }
+};
