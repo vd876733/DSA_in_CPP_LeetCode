b5e8eff6cb9381ef8d484632db3fe7bc704c7bfa
Varad Deshmukh <vd876733@gmail.com>
2026-08-02 16:18:30 +0530

    Time: 7 ms (30.44%), Space: 24 MB (41.97%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        // Check rows
+        for(int i = 0; i < 9; i++) {
+            unordered_set<char> st;
+            for(int j = 0; j < 9; j++) {
+                if(board[i][j] == '.')
+                    continue;
+                if(st.count(board[i][j]))
+                    return false;
+                st.insert(board[i][j]);
+            }
+        }
+        // Check columns
+        for(int j = 0; j < 9; j++) {
+            unordered_set<char> st;
+            for(int i = 0; i < 9; i++) {
+                if(board[i][j] == '.')
+                    continue;
+                if(st.count(board[i][j]))
+                    return false;
+                st.insert(board[i][j]);
+            }
+        }
+        // Check 3x3 boxes
+        for(int row = 0; row < 9; row += 3) {
+            for(int col = 0; col < 9; col += 3) {
+                unordered_set<char> st;
+                for(int i = row; i < row + 3; i++) {
+                    for(int j = col; j < col + 3; j++) {
+                        if(board[i][j] == '.')
+                            continue;
+                        if(st.count(board[i][j]))
+                            return false;
+                        st.insert(board[i][j]);
+                    }
+                }
+            }
+        }
+        return true;
+    }
+};
