1130ef360e62fc08207b0f7a26bbad3e56c78c75
Varad Deshmukh <vd876733@gmail.com>
2026-08-02 23:43:23 +0530

    Time: 72 ms (53.07%), Space: 34.1 MB (47.49%) - LeetHub

---
+++ 2288-apply-discount-to-prices/2288-apply-discount-to-prices.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string discountPrices(string sentence, int discount) {
+        stringstream ss(sentence);
+        string word;
+        string ans = "";
+        while (ss >> word) {
+            bool valid = true;
+            if (word[0] != '$' || word.size() == 1)
+                valid = false;
+            for (int i = 1; i < word.size() && valid; i++) {
+                if (!isdigit(word[i]))
+                    valid = false;
+            }
+            if (valid) {
+                long long value = stoll(word.substr(1));
+                double price = value * (100 - discount) / 100.0;
+                ostringstream out;
+                out << "$" << fixed << setprecision(2) << price;
+                ans += out.str();
+            } else {
+                ans += word;
+            }
+            ans += " ";
+        }
+        ans.pop_back();
+        return ans;
+    }
+};
