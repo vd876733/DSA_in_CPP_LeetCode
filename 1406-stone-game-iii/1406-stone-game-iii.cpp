d84ba4bf2ed652de0c97e22f29c8416a00f6be3a
Varad Deshmukh <vd876733@gmail.com>
2026-08-03 10:22:57 +0530

    Time: 41 ms (39.39%), Space: 141.5 MB (48.07%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.cpp
+++ 1406-stone-game-iii/1406-stone-game-iii.cpp
@@ -1,45 +1,26 @@
 class Solution {
 public:
-
-    int solve(int i, vector<int>& stoneValue,
-              vector<int>& dp) {
-
+    int solve(int i, vector<int>& stoneValue,vector<int>& dp) {
         if (i >= stoneValue.size())
             return 0;
-
         if (dp[i] != INT_MIN)
             return dp[i];
-
         int take = 0;
         int ans = INT_MIN;
-
         for (int k = 0; k < 3 && i + k < stoneValue.size(); k++) {
-
             take += stoneValue[i + k];
-
-            ans = max(ans,
-                      take - solve(i + k + 1,
-                                   stoneValue,
-                                   dp));
+            ans = max(ans,take - solve(i + k + 1,stoneValue,dp));
         }
-
         return dp[i] = ans;
     }
-
     string stoneGameIII(vector<int>& stoneValue) {
-
         int n = stoneValue.size();
-
         vector<int> dp(n, INT_MIN);
-
         int diff = solve(0, stoneValue, dp);
-
         if (diff > 0)
             return "Alice";
-
         if (diff < 0)
             return "Bob";
-
         return "Tie";
     }
 };
