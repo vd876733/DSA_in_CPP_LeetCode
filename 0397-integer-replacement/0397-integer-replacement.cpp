7a2b8f72c93373832fb51bdae984d52d8e3d36dc
Varad Deshmukh <vd876733@gmail.com>
2026-08-29 00:09:55 +0530

    Time: 0 ms (100%), Space: 7.7 MB (79.68%) - LeetHub

---
+++ 0397-integer-replacement/0397-integer-replacement.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int integerReplacement(int n) {
+        long long num = n;
+        int count = 0 ;
+        while( num != 1){
+            if( num % 2 == 0 ){
+                num = num / 2;
+                
+            }else{
+                if (num == 3 || num % 4 == 1) {
+                    num--;
+                }
+                else {
+                    num++;
+                }
+                
+            }
+            count++;
+        }
+        return count ;
+    }
+};
