cfb12c019f86ec7666bc7d1e8ca144a7e7bf6922
Varad Deshmukh <vd876733@gmail.com>
2026-09-01 12:38:14 +0530

    Time: 0 ms (100%), Space: 9.5 MB (15.26%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        unordered_map<char, int> mp;
+        for (char c : s) {
+            mp[c]++;
+        }
+        for (char c : t) {
+            if (mp[c] == 0) {
+                return c;
+            }
+            mp[c]--;
+        }
+        return ' ';
+    }
+};
