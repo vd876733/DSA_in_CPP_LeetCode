aacd746e029c57a4749043d12aa567485d8c8ce4
Varad Deshmukh <vd876733@gmail.com>
2026-09-17 00:14:44 +0530

    Time: 0 ms (100%), Space: 8.7 MB (46.46%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -2,32 +2,23 @@ class Solution {
 public:
     int numDecodings(string s) {
         int n = s.size();
-
         if (s[0] == '0') {
             return 0;
         }
-
         vector<int> dp(n + 1, 0);
-
         dp[0] = 1;
         dp[1] = 1;
-
         for (int i = 2; i <= n; i++) {
-
             // Take one digit
             if (s[i - 1] != '0') {
                 dp[i] += dp[i - 1];
             }
-
             // Take two digits
-            int twoDigit = (s[i - 2] - '0') * 10
-                         + (s[i - 1] - '0');
-
+            int twoDigit = (s[i - 2] - '0') * 10 + (s[i - 1] - '0');
             if (twoDigit >= 10 && twoDigit <= 26) {
                 dp[i] += dp[i - 2];
             }
         }
-
         return dp[n];
     }
 };
